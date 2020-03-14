614bb190f33f1274bf0012d8c4691f4e85df187f
koizr <koizumi.ryo.dev@gmail.com>
2020-03-15 00:22:34 +0900

    test: add test case

--- src/test/scala/com/koizr/cinemacity/usecase/BuyTicketsSpec.scala
+++ src/test/scala/com/koizr/cinemacity/usecase/BuyTicketsSpec.scala
@@ -14,11 +14,26 @@ class BuyTicketsSpec extends FlatSpec {
   def screen(at: DateTime): Screen =
     Screen(Film("The movie", Time(2)), ScreenNumber(1), at)
 
-  "General customer" should "buy a ticket for 1800 yen at Monday 19:00" in {
+  // TODO: TDT にしたい
+  // 2020-01-06 は月曜日
+  "General customer" should "buy a weekday early show ticket for 1800 yen" in {
     val tickets = BuyTicket(group(CustomerType.General), screen(DateTime(2020, 1, 6, 19)))
     assert(tickets.head.price == JPY(1800))
   }
-  "Senior customer" should "buy a ticket for 1100 yen at Monday 19:00" in {
+  it should "buy a weekday late show ticket for 1300 yen" in {
+    val tickets = BuyTicket(group(CustomerType.General), screen(DateTime(2020, 1, 6, 20)))
+    assert(tickets.head.price == JPY(1300))
+  }
+  it should "buy a holiday early show ticket for 1800 yen" in {
+    val tickets = BuyTicket(group(CustomerType.General), screen(DateTime(2020, 1, 5, 19)))
+    assert(tickets.head.price == JPY(1800))
+  }
+  it should "buy a holiday late show ticket for 1300 yen" in {
+    val tickets = BuyTicket(group(CustomerType.General), screen(DateTime(2020, 1, 5, 20)))
+    assert(tickets.head.price == JPY(1300))
+  }
+
+  "Senior customer" should "buy a weekday early show ticket for 1100 yen" in {
     val tickets = BuyTicket(group(CustomerType.Senior), screen(DateTime(2020, 1, 6, 19)))
     assert(tickets.head.price == JPY(1100))
   }
